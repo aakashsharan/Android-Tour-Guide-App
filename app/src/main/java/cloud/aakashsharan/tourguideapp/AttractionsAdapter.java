9e39015719b13ede292c45367c7907e448df746b
Aakash Sharan <akisharan@hotmail.com>
2017-07-11 22:33:33 -0500

    added multiple densities images and commented out ButterKnife library

--- app/src/main/java/cloud/aakashsharan/tourguideapp/AttractionsAdapter.java
+++ app/src/main/java/cloud/aakashsharan/tourguideapp/AttractionsAdapter.java
@@ -17,14 +17,15 @@ import java.util.ArrayList;
  * Created by Aakash on 7/10/2017.
  */
 
-public class AttractionsAdapter extends ArrayAdapter<Attractions>{
+public class AttractionsAdapter extends ArrayAdapter<Attractions> {
 
     private int mColorResourceId;
 
-    public AttractionsAdapter(Activity context, ArrayList<Attractions> attraction, int colorResourceId){
+    public AttractionsAdapter(Activity context, ArrayList<Attractions> attraction, int colorResourceId) {
         super(context, 0, attraction);
         mColorResourceId = colorResourceId;
     }
+
 
     @NonNull
     @Override
@@ -59,5 +60,62 @@ public class AttractionsAdapter extends ArrayAdapter<Attractions>{
 
         return listItemView;
     }
+
+    /*
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder holder;
+        LayoutInflater vi = LayoutInflater.from(getContext());
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = vi.inflate(R.layout.list_item, parent, false);
+            holder = new ViewHolder(listItemView);
+
+        }
+        else{
+            holder = (ViewHolder) listItemView.getTag();
+        }
+        Attractions attraction = getItem(position);
+
+
+        Log.i("holder is: "+holder, "");
+        Log.v("holder is: "+holder, "test");
+
+        Log.i("attraction is: "+attraction.getTitle(), "");
+        Log.v("attraction is: "+attraction.getTitle(), "");
+
+        String a = "ab";
+        holder.title.setText(a);
+
+        //holder.title.setText(attraction.getTitle());
+        holder.description.setText(attraction.getDescription());
+
+
+        if (attraction.hasImage()) {
+            holder.image.setImageResource(attraction.getImageResourceId());
+            holder.image.setVisibility(View.VISIBLE);
+        } else {
+            holder.image.setVisibility(View.GONE);
+        }
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        holder.description.setBackgroundColor(color);
+
+        return listItemView;
+
+    }
+
+    static class ViewHolder {
+        @BindView(R.id.attractions_title)
+        TextView title;
+        @BindView(R.id.description)
+        TextView description;
+        @BindView(R.id.image)
+        ImageView image;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }*/
 
 }
